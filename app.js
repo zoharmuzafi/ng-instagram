7534840e897651b0b72af7d1ddbe19626628a36f
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2016-01-22 13:43:43 -0800

    added pase server

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-var app = angular.module('instagramSearchApp', ['ngRoute']);
+var app = angular.module('instagramSearchApp', ['ngRoute', 'ngResource']);
+
+
+//parsse
+var parseRequestHeaders = {
+  'X-Parse-Application-Id': '6c8RuTlGMBRj7whVDiiIYYZ7Lk98hslI5UgUBvjU',
+  'X-Parse-REST-API-Key': 'JBtMt49ZQEf8CQhD3t9YXq17M3l2nl1OBxc8ZPOH'
+};
+
+//resource
+app.factory('Photo', ['$resource', function ($resource) {
+  return $resource('https://api.parse.com/1/classes/Photo/:photoId', { photoId: '@photoId' },
+    {
+      query: {
+        method: 'GET',
+        isArray: false,
+        headers: parseRequestHeaders
+      },
+      save: {
+        method: 'POST',
+        headers: parseRequestHeaders
+      }
+    });
+}]);
 
 //routes config
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
@@ -19,7 +42,7 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 }]);
 
 //controllers
-app.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
+app.controller('SearchCtrl', ['$scope', '$http', 'Photo', function ($scope, $http, Photo) {
 	$scope.search = function(){
 		var tag = $scope.searchTag.tag;
 		$scope.searchTag = {};
@@ -33,10 +56,22 @@ app.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
 	};
 
 	$scope.savePhoto = function(photo){
-		console.log(photo);
+		var photoData={
+			url: photo.images.standard_resolution.url,
+			username: photo.user.username,
+			likes: photo.likes.count
+		};
+		Photo.save(photoData, function(response){
+			console.log(response);
+		});
 	};
 }]);
 
-app.controller('FavoritesCtrl', ['$scope', function ($scope) {
- 
+app.controller('FavoritesCtrl', ['$scope', 'Photo', function ($scope, Photo) {
+	$scope.favorites = [];
+	Photo.query(function (data) {
+	    $scope.favorites = data;
+	    $scope.favorites = data.results;
+	    console.log($scope.favorites);
+	  });
 }]);
