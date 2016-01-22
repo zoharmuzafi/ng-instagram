d5c4550e8c81d76430a1995bf9aed8242c10fdf9
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2016-01-22 11:36:34 -0800

    added cards and content

--- app.js
+++ app.js
@@ -19,9 +19,18 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 }]);
 
 //controllers
-app.controller('SearchCtrl', ['$scope', function ($scope) {
-	$scope.test = "test";
-
+app.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
+	$scope.search = function(){
+		var tag = $scope.searchTag.tag;
+		$scope.searchTag = {};
+		var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=d8d0d6b44249490bbde6eee4d1968dac&callback=JSON_CALLBACK';
+		$http.jsonp(url)
+      .then(function (response) {
+        $scope.photos = response.data.data;
+        console.log(response);
+     	 }
+      );
+	};
 }]);
 
 app.controller('FavoritesCtrl', ['$scope', function ($scope) {
