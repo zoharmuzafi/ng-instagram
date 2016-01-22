c37f2bfe7203f7ed960ef2c6886998182e0fc58c
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2016-01-22 11:00:45 -0800

    added nav and basic routes

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-var app = angular.module('instagramSearchApp', []);
+var app = angular.module('instagramSearchApp', ['ngRoute']);
+
+//routes config
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/search.html',
+      controller: 'SearchCtrl'
+    })
+    .when('/favorites', {
+      templateUrl: 'templates/favorites.html',
+      controller: 'FavoritesCtrl'
+    });
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}]);
+
+//controllers
+app.controller('SearchCtrl', ['$scope', function ($scope) {
+	$scope.test = "test";
+
+}]);
+
+app.controller('FavoritesCtrl', ['$scope', function ($scope) {
+ 
+}]);
